ccd15b899a9ed6928821497d5888a5dc9868e1c8
Roman Chornyi <ppgora352@gmail.com>
2023-11-29 00:21:15 +0200

    Authorization created

---
+++ life-quality-back/Controllers/Authorization/DatabaseAuthenticationHandler.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace life_quality_back.Controllers.Authorization
+{
+    public class DatabaseAuthenticationHandler : IAuthenticationHandler
+    {
+        private readonly Dictionary<string, (string password, int userId)> userDatabase;
+
+        public DatabaseAuthenticationHandler()
+        {
+            // Наразі стоїть ЗАГЛУШКА
+            // Простий приклад бази даних (логін, пароль та ідентифікатор користувача)
+            //userDatabase = new Dictionary<string, (string, int)>
+            //{
+            //    {"john_doe", ("7c6a180b36896a0a8c02787eeafb0e4c", 0)},
+            //    {"alice", ("6cb75f652a9b52798eb6cf2201057c73", 1)}
+            //};
+            userDatabase = new Dictionary<string, (string, int)>
+            {
+                {"john_doe", ("password1", 0)},
+                {"alice", ("password2", 1)}
+            };
+        }
+        public RespondAnswer Authenticate(string login, string password)
+        {
+            //Хешуємо пароль
+            var hashPassword = HashPassword(Encoding.UTF8.GetBytes(password));
+
+            // Перевірка користувача в базі даних
+            if (userDatabase.ContainsKey(login) && userDatabase[login].password == hashPassword)
+            {
+                return new RespondAnswer
+                {
+                    isOperationSuccess = true,
+                    idUser = userDatabase[login].userId,
+                    outcomeMessage = "Authorization complete successfully!"
+                };
+            }
+
+            return new RespondAnswer
+            {
+                isOperationSuccess = false,
+                idUser = -1,
+                outcomeMessage = "Incorrect login or password"
+            };
+        }
+
+        private string HashPassword(byte[] password)
+        {
+            var md5Result = MD5Algorythm.Algorithm(password);
+
+            var result = MD5Algorythm.MakeStringFromArrUInt(md5Result);
+
+            return result;
+        }
+    }
+}
---
+++ life-quality-back/Controllers/Authorization/RespondAnswer.cs
@@ -0,0 +1,9 @@
+﻿namespace life_quality_back.Controllers.Authorization
+{
+    public struct RespondAnswer
+    {
+        public bool isOperationSuccess;
+        public int idUser;
+        public string outcomeMessage;
+    }
+}
